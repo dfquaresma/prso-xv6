8aa7a6ce992a669eaafc4d16fe81c013bc38f735
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-04-03 09:45:22 -0300

    Fix nice's setpriority function

--- lab1/nice.c
+++ lab1/nice.c
@@ -6,7 +6,14 @@ main(int argc, char *argv[])
 {
   int pid = atoi(argv[1]);
   int prio = atoi(argv[2]);
-  int oldprio = setpriority(pid, prio);
+
+  int oldprio;
+  if (prio == 0 && *argv[2] != '0') {
+    oldprio = -1;
+  } else {
+    oldprio = setpriority(pid, prio);
+  }
+  
   printf(1, "%d\n", oldprio);
   return exit();
 }
