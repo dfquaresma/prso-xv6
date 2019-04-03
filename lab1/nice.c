2b166797a8a183ed337a255be087e11c3b21fd02
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-04-03 09:00:12 -0300

    Add print of oldprio

--- lab1/nice.c
+++ lab1/nice.c
@@ -5,8 +5,9 @@ int
 main(int argc, char *argv[])
 {
   int pid = atoi(argv[1]);
-  int prio = atoi(argv[2]); 
+  int prio = atoi(argv[2]);
   int oldprio = setpriority(pid, prio);
+  printf(1, "%d\n", oldprio);
   return exit();
 }
 
