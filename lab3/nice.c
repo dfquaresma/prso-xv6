d8718843e6424ab847281eb2f83758c23114e517
Emanuel Martins <emanueljoivo@gmail.com>
2019-04-05 13:12:24 -0300

    Update nice.c from master manually

--- lab3/nice.c
+++ lab3/nice.c
@@ -5,8 +5,16 @@ int
 main(int argc, char *argv[])
 {
   int pid = atoi(argv[1]);
-  int prio = atoi(argv[2]); 
-  int oldprio = setpriority(pid, prio);
+  int prio = atoi(argv[2]);
+
+  int oldprio;
+  if (prio == 0 && *argv[2] != '0') {
+    oldprio = -1;
+  } else {
+    oldprio = setpriority(pid, prio);
+  }
+  
+  printf(1, "%d\n", oldprio);
   return exit();
 }
 
