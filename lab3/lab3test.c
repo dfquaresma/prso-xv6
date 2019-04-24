638cad17dd8b7c2484a961888b0a63b5d7eec70e
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-04-24 08:37:27 -0300

    Update lab3test

--- lab3/lab3test.c
+++ lab3/lab3test.c
@@ -6,14 +6,14 @@ main(int argc, char *argv[])
 {   
   int prio = atoi(argv[1]);
   int pid  = getpid();
+
   setpriority(pid, prio);
-  
+  printf(2, "pid:%d prio:%d\n$ ", pid, prio);
+  int count = 0;
   while (1) {
-      sleep(100);
-      printf(1, "%d\n$ ", prio);
+    count = (count + 1) % 1000;
   }
 
   return exit();
-
-
+  
 }
